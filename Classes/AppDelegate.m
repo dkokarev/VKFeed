cfb943705c7d988b12943d87f9b036b057bfdd59
dkokarev <danny@MacBook-Pro-danny.local>
2016-03-04 17:16:13 +0200

    Minor fixes

--- Classes/AppDelegate.m
+++ Classes/AppDelegate.m
@@ -17,7 +17,7 @@
 
 static NSString * const VK_APP_ID = @"5290047";
 
-@interface AppDelegate () //<VKSdkDelegate>
+@interface AppDelegate ()
 
 @end
 
@@ -29,29 +29,9 @@ static NSString * const VK_APP_ID = @"5290047";
     UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:controller];
     self.window.rootViewController = navController;
     [self.window makeKeyAndVisible];
-    /*
-    VKSdk *vksdk = [VKSdk initializeWithAppId:VK_APP_ID];
-    [vksdk registerDelegate:self];
-    [VKSdk wakeUpSession:@[@"wall"] completeBlock:^(VKAuthorizationState state, NSError *error) {
-        if (state == VKAuthorizationAuthorized) {
-            [self showControllerWithClass:[FeedViewController class]];
-        } else {
-            [self showControllerWithClass:[LoginViewController class]];
-        }
-    }];
-     */
     return YES;
 }
-/*
-- (void)showControllerWithClass:(Class)class {
-    if (!class) {
-        return;
-    }
-    UIViewController *controller = [[class alloc] init];
-    UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:controller];
-    self.window.rootViewController = navController;
-}
-*/
+
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
@@ -73,35 +53,5 @@ static NSString * const VK_APP_ID = @"5290047";
 - (void)applicationWillTerminate:(UIApplication *)application {
     [MagicalRecord cleanUp];
 }
-/*
-- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options {
-    [VKSdk processOpenURL:url fromApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey]];
-    return YES;
-}
 
-- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
-    [VKSdk processOpenURL:url fromApplication:sourceApplication];
-    return YES;
-}
-
-#pragma mark - VKSdkDelegate
-
-- (void)vkSdkAccessAuthorizationFinishedWithResult:(VKAuthorizationResult *)result {
-    if (result.error) {
-        [UIAlertController presentWithMessage:result.error.localizedDescription inController:self.window.rootViewController];
-        return;
-    }
-    [self showControllerWithClass:[FeedViewController class]];
-}
-
-- (void)vkSdkUserAuthorizationFailed {
-    [UIAlertController presentWithMessage:@"Authorization failed" inController:self.window.rootViewController];
-}
-
-- (void)vkSdkAccessTokenUpdated:(VKAccessToken *)newToken oldToken:(VKAccessToken *)oldToken {
-    if (!newToken && oldToken) {
-        [self showControllerWithClass:[LoginViewController class]];
-    }
-}
-*/
 @end
